83720c70747ffd913c1726e2c92a55f041c20ae3
saintbreezy-cyber <saintbreezy97@gmail.com>
2026-09-16 14:53:54 -0700

    offline PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'CHIMUKA SOFTWARES',
+        short_name: 'CHIMUKA',
+        description: 'Chimuka School Management System',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      }
+    })
+  ],
 })
